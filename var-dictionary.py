68513bb404b04505d94781c6336a6b7d09e545ae
Sherali Jurayev <100849791+SheraliJurayev@users.noreply.github.com>
2023-01-13 14:57:18 +0500

    dictionary o'zgaruvchisi haqida

---
+++ var-dictionary.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 14:08:38 2023
+
+@author: Sh_Jurayeff
+"""
+    #Oilam
+#otam={'ismi':'normurod' , 'familiyasi':'avloqulov' , 't_yili':'1972' ,'kasbi':'usta'}
+#onam={'ismi':'nargiza' , 'familiyasi':'rahmonova' , 't_yili':'1973' ,'kasbi':'kotiba'}
+#akam_1={'ismi':'shukurullo' , 'familiyasi':'jurayev' , 't_yili':'1995' ,'kasbi':"o'qituvchi"}
+#akam_2={'ismi':'elshod' , 'familiyasi':'jurayev' , 't_yili':'1997' ,'kasbi':"programmer"}
+#men={'ismi':'sherali' , 'familiyasi':'jurayev' , 't_yili':'2002' ,'kasbi':'programmer'}
+#print(f"Otamning ismi {otam['ismi'].title()} , {otam['t_yili']}-yilda tug'ilgan , kasbi {otam['kasbi']}. ")
+#print(f"Onamning ismi {onam['ismi'].title()} , {onam['t_yili']}-yilda tug'ilgan , kasbi {onam['kasbi']} ")
+#print(f"Katta akamning ismi {akam_1['ismi'].title()} , {akam_1['t_yili']}-yilda tug'ilgan , kasbi {akam_1['kasbi']}.")
+#print(f"Kichik akamning ismi {akam_2['ismi'].title()} , {akam_2['t_yili']}-yilda tug'ilgan , kasbi {akam_2['kasbi']}. ")
+#print(f"Mening ismim {men['ismi'].title()} , {men['t_yili']}-yilda tug'ilganman , kasbim {men['kasbi']}. ")
+
+    #Python dictionary
+#python_dictionary={'int':"butun sonlar to'plami" , 
+#                   'float':"unlik sonlar tuplami" ,
+#                   'for':"sikl operatori",
+#                   'if-else':"shart operatori",
+#                   }
+#print(python_diytionary['int'])
+#dictionary= python_diytionary.get('int' , "bunday atama mavjud emas")
+#print(dictionary)
+#dict=input("Kiriting:")
+
+#print(python_diytionary[dict])
+
+#key = input("Kalit so'z kiriting:").lower()
+#dict = python_dictionary.get(key)
+#if dict==None:
+#    print("Bunday so'z mavjud emas")
+#else:
+#    print(f"{key.title()} so'zi {dict} deb tarjima qilinadi")
+
+
